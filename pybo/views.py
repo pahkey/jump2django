5545b738c67365aa11e2d8248254dc9687fa5079
pahkey <pahkey@gmail.com>
2022-04-01 10:24:19 +0900

    2-06

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .models import Question, Answer
 
 
 def index(request):
@@ -12,3 +14,10 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    return redirect('pybo:detail', question_id=question.id)
