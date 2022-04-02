286f6f017ecc7522dd89965a2f675b8158d13048
pahkey <pahkey@gmail.com>
2022-04-03 01:05:58 +0900

    3-11

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -53,3 +53,13 @@ def answer_delete(request, answer_id):
     else:
         answer.delete()
     return redirect('pybo:detail', question_id=answer.question.id)
+
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id)
