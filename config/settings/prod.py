e1cb5f4eadd85ead649a3975417e5884b0ffe53e
pahkey <pahkey@gmail.com>
2022-04-03 17:10:01 +0900

    hosts

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ import environ
 
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.58.70', 'pybo.kr']
+ALLOWED_HOSTS = ['3.37.58.70', 'pybo.kr', 'django.pybo.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
