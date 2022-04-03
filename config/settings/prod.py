d34571a0b5ffc8f3670f1eca7d017877270ffedd
pahkey <pahkey@gmail.com>
2022-04-03 15:09:57 +0900

    database

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['3.37.58.70', 'pybo.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '<데이터베이스암호>',
+        'HOST': '<데이터베이스주소>',
+        'PORT': '5432',
+    }
+}
