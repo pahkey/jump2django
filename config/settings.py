a66f6b27e709a51758b10f29aab2e68a596e4239
pahkey <pahkey@gmail.com>
2022-04-03 12:22:31 +0900

    4-07

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%(m4+u+7qh8-vf=04@jx3)=kz)$62wmnkx3*_%ppn(otje832m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.37.58.70']
 
 
 # Application definition
