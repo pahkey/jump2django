d25d8655d70fd683b91438ff994f5668a3abff54
pahkey <pahkey@gmail.com>
2022-04-03 15:20:30 +0900

    env

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,6 @@
+import environ
+import os
+
 from .base import *
 
 ALLOWED_HOSTS = ['3.37.58.70', 'pybo.kr']
@@ -5,13 +8,17 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pybo',
         'USER': 'dbmasteruser',
-        'PASSWORD': '<데이터베이스암호>',
-        'HOST': '<데이터베이스주소>',
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
         'PORT': '5432',
     }
 }
