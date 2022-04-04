2b777b9966c32b67f78554b7892549be0e61265b
pahkey <pahkey@gmail.com>
2022-04-04 10:28:32 +0900

    hosts

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -13,7 +13,7 @@ def index(request):
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목
             Q(content__icontains=kw) |  # 내용
-            Q(answer__content__icontains=kw) |  # 답변내용
+            Q(answer__content__icontains=kw) |  # 답변 내용
             Q(author__username__icontains=kw) |  # 질문 글쓴이
             Q(answer__author__username__icontains=kw)  # 답변 글쓴이
         ).distinct()
